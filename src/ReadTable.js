6e74c3134357ccf2319344f0b51a29d7aa93fdfa
KeerthikaChandra <keerthikachandrashekhar0410@gmail.com>
2022-07-12 13:17:51 +0530

    frontend commit

--- src/ReadTable.js
+++ src/ReadTable.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const ReadTable = ({posts, handleEditPostForm}) => {
-    return (
-        <>
-             {posts.map((post) => 
-                    <tr key={post.id}>
-                        <td>{post.id}</td>
-                        <td>{post.sprint}</td>
-                        <td>{post.name}</td>
-                        <td>{post.wentwell}</td>
-                        <td>{post.wentwrong}</td>
-                        <td>{post.focusarea}</td>
-                        <td>{post.remarks}</td>
-                        <td><button 
-                type="button"
-                className="me-3 btn btn-primary ml-auto d-block mb-2"
-                data-bs-toggle="modal"
-                data-bs-target="#editModalForm"
-                onClick={(e) => handleEditPostForm(e, post)}
-                >
-                    Edit
-                </button></td>
-                    </tr>
-                    )}
-        </>
-    )
-}
+const ReadTable = ({ posts, handleEditPostForm, handleDelete }) => {
+  return (
+    <>
+      {posts.map((post) => (
+        <tr key={post._id}>
+          <td>{post.sprint}</td>
+          <td>{post.name}</td>
+          <td>{post.wentwell}</td>
+          <td>{post.wentwrong}</td>
+          <td>{post.focusarea}</td>
+          <td>{post.remarks}</td>
+          <td>
+            <button
+              type="button"
+              className="btn btn-primary ms-1 btn-md"
+              data-bs-toggle="modal"
+              data-bs-target="#editModalForm"
+              onClick={(e) => handleEditPostForm(e, post)}
+            >
+              {" "}
+              Edit{" "}
+            </button>
+            <button
+              type="submit"
+              className="btn btn-danger btn-md ms-3"
+              onClick={() => handleDelete(post._id)}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
 
-export default ReadTable
+export default ReadTable;
--- src/Table.js
+++ src/Table.js
@@ -1,214 +1,235 @@
-import React, {useState, useEffect} from 'react';
-// import axios from 'axios';
-import ReadTable from './ReadTable';
-import nextId from "react-id-generator";
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import axios, { Axios } from "axios";
+import ReadTable from "./ReadTable";
+import styled from "styled-components";
 
 const StyledDiv = styled.div`
-    text-align: center;
-    font-size: 30px;
-`
-
+  text-align: center;
+  font-size: 30px;
+`;
 
 const Table = () => {
+  const [searchPost, setSearchPost] = useState('');
+  const [posts, setPosts] = useState([]);
 
-const [searchQuery, setSearchQuery] = useState("")
-const [posts, setPosts] = useState([])
+  const [addPost, setAddPost] = useState({
+    sprint: "",
+    name: "",
+    wentwell: "",
+    wentwrong: "",
+    focusarea: "",
+    remarks: "",
+  });
 
-const [addPost, setAddPost] = useState({
-    id: '',
-    sprint: '',
-    name: '',
-    wentwell: '',
-    wentwrong: '',
-    focusarea: '',
-    remarks: ''
-})
+  // Get ID
+  const [editPostId, setEditPostId] = useState(null);
 
-// Get ID
-const [editPostId, setEditPostId] = useState(null)
+  const [editFormData, setEditFormData] = useState({
+    sprint: "",
+    name: "",
+    wentwell: "",
+    wentwrong: "",
+    focusarea: "",
+    remarks: "",
+  });
 
-const [editFormData, setEditFormData] = useState({
-    id: addPost.id,
-    sprint: addPost.sprint,
-    name: addPost.name,
-    wentwell: addPost.wentwell,
-    wentwrong: addPost.wentwrong,
-    focusarea: addPost.focusarea,
-    remarks: addPost.remarks
-})
+  // Get form values
+  const handleChange = (input) => (e) => {
+    e.preventDefault();
+    console.log(addPost);
+    setAddPost({ ...addPost, [input]: e.target.value });
+  };
 
-// Get form values
-const handleChange = (input) => (e) => {
-    e.preventDefault()
-    console.log(addPost)
-    setAddPost({...addPost, [input]: e.target.value})
-}
-
-// Add data to table
-const handleAddPost = (e) => {
-    e.preventDefault()
-    e.target.reset()
-    const newPost = {
-        id: nextId(),
-        sprint: addPost.sprint,
-        name: addPost.name,
-        wentwell: addPost.wentwell,
-        wentwrong: addPost.wentwrong,
-        focusarea: addPost.focusarea,
-        remarks: addPost.remarks
-    }
-
-    const newPosts = [...posts, newPost]
-    setPosts(newPosts)
-}
-
-
-//edit form data
-const handleEditPostForm = (e, post) => {
-    e.preventDefault()
-    setEditPostId(post.id)
+  //edit form data
+  const handleEditPostForm = (e, post) => {
+    e.preventDefault();
+    setEditPostId(post._id);
 
     const formValues = {
-        id: post.id,
-        sprint: post.sprint,
-        name: post.name,
-        wentwell: post.wentwell,
-        wentwrong: post.wentwrong,
-        focusarea: post.focusarea,
-        remarks: post.remarks
-    }
+      sprint: post.sprint,
+      name: post.name,
+      wentwell: post.wentwell,
+      wentwrong: post.wentwrong,
+      focusarea: post.focusarea,
+      remarks: post.remarks,
+    };
 
-    setEditFormData(formValues)
-}
+    setEditFormData(formValues);
+  };
 
-//Edit data value
-const handleEditFormClick = (input) => (e) => {
-    e.preventDefault()
-    setEditFormData({...editFormData, [input]: e.target.value})
-}
+  //Edit data value
+  const handleEditFormClick = (input) => (e) => {
+    e.preventDefault();
+    setEditFormData({ ...editFormData, [input]: e.target.value });
+  };
 
-//Save form
-const handleFormSave = (e) => {
-    e.preventDefault()
+  //search filter data
+
+  // function search() {
+  //   return posts.filter(
+  //     (row) => row.title.toLowerCase().indexOf(searchQuery) > -1
+  //   );
+  // }
+
+  const fetchUrl = "https://scrum-tracker-api.herokuapp.com/getScrumdata";
+
+  async function fetchData() {
+    const data = await axios.get(fetchUrl);
+    console.log("data", data);
+    setPosts(data.data.data);
+  }
+
+  //delete function
+
+  async function handleDelete(id) {
+    await axios.delete("https://scrum-tracker-api.herokuapp.com/delete/" + id);
+    await fetchData();
+  }
+
+  // Add data to table
+
+  const handleAddPost = async (e) => {
+    e.preventDefault();
+    e.target.reset();
+    const newPost = {
+      sprint: addPost.sprint,
+      name: addPost.name,
+      wentwell: addPost.wentwell,
+      wentwrong: addPost.wentwrong,
+      focusarea: addPost.focusarea,
+      remarks: addPost.remarks,
+    };
+
+    await axios.post(`https://scrum-tracker-api.herokuapp.com/postScrumdata`, newPost);
+
+    await fetchData();
+  };
+
+  //Save form
+  const handleFormSave = async (e) => {
+    e.preventDefault();
 
     const savePost = {
-        id: editFormData.id,
-        sprint: editFormData.sprint,
-        name: editFormData.name,
-        wentwell: editFormData.wentwell,
-        wentwrong: editFormData.wentwrong,
-        focusarea: editFormData.focusarea,
-        remarks: editFormData.remarks
-    }
+      sprint: editFormData.sprint,
+      name: editFormData.name,
+      wentwell: editFormData.wentwell,
+      wentwrong: editFormData.wentwrong,
+      focusarea: editFormData.focusarea,
+      remarks: editFormData.remarks,
+    };
 
-    const newPosts = [...posts]
+    await axios.put(`https://scrum-tracker-api.herokuapp.com/update/${editPostId}`, savePost);
+    await fetchData();
+  };
 
-    const formIndex = posts.findIndex((post) => post.id === editPostId)
-    newPosts[formIndex] = savePost
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    setPosts(newPosts)
-    setEditPostId(null)
-    console.log(editPostId);
-}
+  // const handleSearch = (input) => (e) => {
+  //  setSearchPost({[input]: e.target.value})
+  // };
 
-//Delete data
+  // const filteredPosts = async () => {
+  //   if(posts){
+  //     let searchedResult;
+  //       searchedResult = posts.map((post) => {
+  //       post.name.toLowerCase().includes(searchPost.toLowerCase()) ||
+  //       post.wentwell.toLowerCase().includes(searchPost.toLowerCase()) ||
+  //       post.wentwrong.toLowerCase().includes(searchPost.toLowerCase()) ||
+  //       post.focuarea.toLowerCase().includes(searchPost.toLowerCase()) ||
+  //       post.remarks.toLowerCase().includes(searchPost.toLowerCase())
+  //    })
+  //    console.log("searchedResult",searchedResult) 
+  //   }
+  //   else{
+  //     console.log("error!")
+  //   }
+   
+  // }
 
-const handleDelete = (e) => {
-    e.preventDefault()
-
-    const newPosts = [...posts]
-
-    const formIndex = posts.findIndex((post) => post.id === editPostId)
-
-    newPosts.splice(formIndex, 1)
-
-    setPosts(newPosts)
-}
-
-//search filter data
-
-function search() {
-    return posts.filter(row => row.title.toLowerCase().indexOf(searchQuery) > -1)
-}
+  // const filteredPosts = () => {
+  //   const p = posts.filter(post => Object.values(post).some(val => val.toLowerCase().includes(searchPost.toLowerCase())));
+  //   console.log("p",p);
+  // }
     
-//get data from json placeholder
-// const fetchUrl = "https://jsonplaceholder.typicode.com/posts"
-// useEffect(() => {
-//     async function fetchData() {
-//         const data = await axios.get(fetchUrl)
-//         setPosts(data.data)
-//     }
-//     fetchData()
-// },[fetchUrl])
-// console.log(posts);
+  // filteredPosts();
 
-    return (
-        <div>
-            <StyledDiv>SCRUM RETRO</StyledDiv>
-            <StyledDiv>RBB</StyledDiv>
-            <div className="d-flex flex-row">
-                <button 
-                type="button"
-                className="me-3 btn btn-primary ml-auto d-block mb-2"
-                data-bs-toggle="modal"
-                data-bs-target="#addModalForm">
-                    Add Data+
-                </button>
-                <form className='row g-3 ms-auto'>
-                    <div className='col-auto'>
-                        <input
-                         type="text"
-                         className="form-control ms-auto"
-                         placeholder="Search data"
-                         value={searchQuery}
-                         onChange={(e) => setSearchQuery(e.target.value)}
-                         />
-                    </div>
-                </form>
-            </div>
-            <table className="table table-bordered border-primary table-responsible">
-                <thead>
-                    <tr>
-                        <th scope="col">Row Id</th>
-                        <th scope="col">Sprint</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">What went well</th>
-                        <th scope="col">What went wrong</th>
-                        <th scope="col">Focus Area</th>
-                        <th scope="col">Additional Remarks</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                   <ReadTable 
-                //    posts={search(posts)}
-                posts={posts}
-                   handleEditPostForm={handleEditPostForm} />
-                </tbody>
-            </table>
+  return (
+    <div>
+      <StyledDiv>SCRUM RETRO</StyledDiv>
+      <StyledDiv>RBB</StyledDiv>
+      <div className="d-flex flex-row">
+        <button
+          type="button"
+          className="me-3 btn btn-primary ml-auto d-block mb-2"
+          data-bs-toggle="modal"
+          data-bs-target="#addModalForm"
+        >
+          Add Data +
+        </button>
+        {/* <form className="row g-3 ms-auto">
+          <div className="col-auto">
+            <input
+              type="text"
+              className="form-control ms-auto"
+              name="search"
+              placeholder="Search data"
+              // value={}
+              onChange={handleSearch("search")}
+            />
+          </div>
+        </form> */}
+      </div>
+      <table className="table table-bordered border-primary table-responsible">
+        <thead>
+          <tr>
+            {/* <th scope="col">Row Id</th> */}
+            <th scope="col">Sprint</th>
+            <th scope="col">Name</th>
+            <th scope="col">What went well</th>
+            <th scope="col">What went wrong</th>
+            <th scope="col">Focus Area</th>
+            <th scope="col">Additional Remarks</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts && (
+            <ReadTable
+              //    posts={search(posts)}
+              posts={posts}
+              // posts={filteredPosts}
+              handleEditPostForm={handleEditPostForm}
+              handleDelete={handleDelete}
+            />
+          )}
+        </tbody>
+      </table>
 
-            {/*Add Modal*/}
-            <div className="modal fade" id="addModalForm" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      {/*Add Modal*/}
+      <div
+        className="modal fade"
+        id="addModalForm"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Add New Post</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h5 className="modal-title" id="exampleModalLabel">
+                Add New Post
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body">
               <form onSubmit={handleAddPost}>
-              <div className="mb-3">
-                  <label className="form-label">Row Id</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="id"
-                    placeholder={addPost.id}
-                    disabled
-                    onChange={handleChange("id")}
-                  />
-                </div>
                 <div className="mb-3">
                   <label className="form-label">Sprint</label>
                   <input
@@ -280,57 +301,65 @@ function search() {
                   ></textarea>
                 </div>
                 <div className="modal-footer d-block">
-                  <button type="submit" data-bs-dismiss="modal" className="btn btn-warning float-end">Submit</button>
+                  <button
+                    type="submit"
+                    data-bs-dismiss="modal"
+                    className="btn btn-warning float-end"
+                  >
+                    Submit
+                  </button>
                 </div>
               </form>
             </div>
           </div>
         </div>
       </div>
-        
-        {/*Edit Row Modal */}
-      <div className="modal fade" id="editModalForm" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Edit Row</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div className="modal-body">
-            <form onSubmit={handleFormSave}>
-              <div className="mb-3">
-                <label className="form-label">Row ID</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="id"
-                  value={editFormData.id}
-                  onChange={handleEditFormClick("id")}
-                  disabled
-                />
-              </div>
-              <div className="mb-3">
-                <label className="form-label">Sprint</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="sprint"
-                  value={editFormData.sprint}
-                  onChange={handleEditFormClick("sprint")}
-                />
-              </div>
-              <div className="mb-3">
-                <label className="form-label">Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="name"
-                  value={editFormData.name}
-                  onChange={handleEditFormClick("name")}
-                />
-              </div>
-              <div className="mb-3">
-              <label className="form-label">What went well</label>
+
+      {/*Edit Row Modal */}
+      <div
+        className="modal fade"
+        id="editModalForm"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Edit Row
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleFormSave}>
+                <div className="mb-3">
+                  <label className="form-label">Sprint</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="sprint"
+                    value={editFormData.sprint}
+                    onChange={handleEditFormClick("sprint")}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    value={editFormData.name}
+                    onChange={handleEditFormClick("name")}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">What went well</label>
                   <textarea
                     rows="4"
                     cols="30"
@@ -341,7 +370,7 @@ function search() {
                   ></textarea>
                 </div>
                 <div className="mb-3">
-              <label className="form-label">What went wrong</label>
+                  <label className="form-label">What went wrong</label>
                   <textarea
                     rows="4"
                     cols="30"
@@ -352,7 +381,7 @@ function search() {
                   ></textarea>
                 </div>
                 <div className="mb-3">
-              <label className="form-label">Focus Area</label>
+                  <label className="form-label">Focus Area</label>
                   <textarea
                     rows="4"
                     cols="20"
@@ -363,7 +392,7 @@ function search() {
                   ></textarea>
                 </div>
                 <div className="mb-3">
-              <label className="form-label">Additional remarks</label>
+                  <label className="form-label">Additional remarks</label>
                   <textarea
                     rows="4"
                     cols="20"
@@ -373,27 +402,29 @@ function search() {
                     onChange={handleEditFormClick("remarks")}
                   ></textarea>
                 </div>
-              <div className="modal-footer d-block">
-                <button
-                  type="submit"
-                  data-bs-dismiss="modal"
-                  className="btn btn-success float-end"
-                >Save Row</button>
+                <div className="modal-footer d-block">
+                  <button
+                    type="submit"
+                    data-bs-dismiss="modal"
+                    className="btn btn-success float-end"
+                  >
+                    Save Row
+                  </button>
 
-                <button
+                  {/* <button
                   type="submit"
                   data-bs-dismiss="modal"
                   className="btn btn-danger float-start"
                   onClick={handleDelete}
-                >Delete Row</button>
-              </div>
-            </form>
+                >Delete Row</button> */}
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-    )
-}
+  );
+};
 
-export default Table
+export default Table;
